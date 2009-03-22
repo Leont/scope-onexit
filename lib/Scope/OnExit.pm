ff28f7049d04fc97d483dfdcce430f4f1c6fff9c
Leon Timmermans <fawaka@gmail.com>
2009-03-22 14:55:48 +0100

    Initial setup

---
+++ lib/Scope/OnExit.pm
@@ -0,0 +1,107 @@
+package Scope::OnExit;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Scope::OnExit - The great new Scope::OnExit!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Scope::OnExit;
+
+    my $foo = Scope::OnExit->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Leon Timmermans, C<< <leont at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-scope-onexit at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Scope-OnExit>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Scope::OnExit
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Scope-OnExit>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Scope-OnExit>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Scope-OnExit>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Scope-OnExit>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Leon Timmermans, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Scope::OnExit
